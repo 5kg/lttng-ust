97d1d66ea5592d11fc5a89d12a1cd3705dcf0919
Zifei Tong <soariez@gmail.com>
2013-09-13 17:06:40 +0800

    Add function to trigger specific tracepoint at runtime

--- liblttng-ust/lttng-dynamic-probes.c
+++ liblttng-ust/lttng-dynamic-probes.c
@@ -20,19 +20,33 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <lttng/tracepoint-types.h>
 #include <lttng/ust-events.h>
 #include <lttng/ringbuffer-config.h>
 
 void lttng_dynamic_probe_callback(struct lttng_event *event)
 {
-    struct lttng_channel *chan = event->chan;
-    struct lttng_ust_lib_ring_buffer_ctx ctx;
-    int ret;
+	struct lttng_channel *chan = event->chan;
+	struct lttng_ust_lib_ring_buffer_ctx ctx;
+	int ret;
 
-    lib_ring_buffer_ctx_init(&ctx, chan->chan, event, 0, 0, -1, chan->handle);
-    ctx.ip = __builtin_return_address(0);
-    ret = chan->ops->event_reserve(&ctx, event->id);
-    if (ret < 0)
-        return;
-    chan->ops->event_commit(&ctx);
+	lib_ring_buffer_ctx_init(&ctx, chan->chan, event, 0, 0, -1, chan->handle);
+	ctx.ip = __builtin_return_address(0);
+	ret = chan->ops->event_reserve(&ctx, event->id);
+	if (ret < 0)
+		return;
+	chan->ops->event_commit(&ctx);
+}
+
+void tracepoint_of(const struct tracepoint* tracepoint)
+{
+	if (caa_unlikely(tracepoint->state)) {
+		struct tracepoint_probe *tp_probe = tracepoint->probes;
+
+		do {
+			void (*tp_cb)(void) = tp_probe->func;
+			void *tp_data = tp_probe->data;
+			URCU_FORCE_CAST(void (*)(struct lttng_event*), tp_cb)(tp_data);
+		} while ((++tp_probe)->func);
+	}
 }
